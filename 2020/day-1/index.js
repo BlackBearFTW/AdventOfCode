c259d1e90cf5f94bf5811b4a8c88248f68cf7737
BlackBearFTW <robinmager@home.nl>
2020-12-02 23:17:40 +0100

    Update index.js

--- 2020/day-1/index.js
+++ 2020/day-1/index.js
@@ -15,15 +15,47 @@ fs.readFile("input.txt", (err, content) => {
             console.log(`${valueLeft} + ${i} = ${valueLeft + i}`);
             console.log(`${valueLeft} * ${i} = ` + valueLeft * i);
 
-            // Part 2
-            console.log('\nPart 2');
-            console.log('404, Solution not found');
-
             // End For loop
             break;
         }
 
     }
+
+
+    // Part 2
+    // Turns input into array
+    const list = content.toString().split("\n").map((elt) => parseInt(elt, 10))
+
+    let found = false
+
+    // Loop through all the items
+    for (const item of list){
+
+        if (found) break
+
+        // look through all items that arent current item
+        for (const item2 of list.filter(elt => elt !== item)){
+
+            if (found) break
+
+            // loop through all items that arent current item or item2
+            for (const item3 of list.filter(elt => elt !== item && elt !==item2))
+
+                if (item + item2 + item3 === 2020){
+
+                    console.log('Part 2');
+                    console.log(`${item} + ${item2} + ${item3} = 2020`)
+                    console.log(`${item} * ${item2} * ${item3} = ` + item*item2*item3)
+                    found = true
+                    break;
+                }
+
+        }
+    }
+
+
+
+
 
 });
 
