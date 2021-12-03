cf45ea2d2c5efe8b9a8a2c447a410724d3793d45
Robin Mager <robinmager@home.nl>
2021-12-03 18:24:23 +0100

    Day 3 Part 2 2021 Finished

--- 2021/Day-3/Day3.cs
+++ 2021/Day-3/Day3.cs
@@ -52,7 +52,56 @@ namespace _2021.Day_3
 
         public void Puzzle_2()
         {
+            string OxygenRatingBinary = filterForCommon(splittedInput,1, 'M');
+            string Co2ScrubberRatingBinary = filterForCommon(splittedInput,0, 'L');
 
-        }
+            Console.WriteLine($"Oxygen: {Convert.ToInt32(OxygenRatingBinary, 2)} ({OxygenRatingBinary})");
+            Console.WriteLine($"CO2 Scrubbing: {Convert.ToInt32(Co2ScrubberRatingBinary, 2)} ({Co2ScrubberRatingBinary})");
+            Console.WriteLine($"{Convert.ToInt32(OxygenRatingBinary, 2)} X {Convert.ToInt32(Co2ScrubberRatingBinary, 2)} = {Convert.ToInt32(Co2ScrubberRatingBinary, 2) * Convert.ToInt32(OxygenRatingBinary, 2)}");
+
+            string filterForCommon(List<string> list, int prefer, char MostOrLeastCommon)
+            {
+                List<string> filteredList = new List<string>(list);
+
+                for (int i = 0; i < (filteredList.First().Trim().Length); i++)
+                {
+                    int zeroCount = 0;
+                    int oneCount = 0;
+
+                    foreach (string line in filteredList)
+                    {
+                        int firstNumber = Int32.Parse(line.ElementAt(i).ToString().TrimEnd());
+
+                        if (firstNumber == 1)
+                        {
+                            oneCount++;
+                            continue;
+                        }
+
+                        zeroCount++;
+                    }
+
+                    if (MostOrLeastCommon == 'M')
+                    {
+                        if (oneCount > zeroCount) filteredList.RemoveAll(x => x.ElementAt(i) == '0'); // More 1's 
+                        if (zeroCount > oneCount) filteredList.RemoveAll(x => x.ElementAt(i) == '1'); // More 0's 
+                    } 
+                    else if (MostOrLeastCommon == 'L')
+                    {
+                        if (oneCount < zeroCount) filteredList.RemoveAll(x => x.ElementAt(i) == '0'); // More 0's  
+                        if (zeroCount < oneCount) filteredList.RemoveAll(x => x.ElementAt(i) == '1'); // More 1's 
+                    }
+
+
+
+                    char filterFor = (prefer == 1) ? '0' : '1';
+                    if (zeroCount == oneCount) filteredList.RemoveAll(x => x.ElementAt(i) == filterFor); // More 0's 
+                    if (filteredList.Count() == 1) break;
+                }
+
+                return filteredList.First().Trim();
+            }
+
+        } // PZ2
     }
 }
